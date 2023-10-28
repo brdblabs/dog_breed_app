b5f60af10739264099d5c593bea473d0ab646c45
brdblabs <96331019+brdblabs@users.noreply.github.com>
2023-10-28 13:00:59 +0400

    Update dog_breed.py

--- dog_breed.py
+++ dog_breed.py
@@ -5,6 +5,8 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.preprocessing import image
 from helper_functions import get_pred_label
 # from helper_functions import create_data_batches
 
@@ -20,51 +22,31 @@ def main():
     # Progress bar while model is running
     st.write('Please standby while your image is analyzed...')
 
+    # Load breed labels
+    labels_csv = pd.read_csv("labels.csv")
+    labels = labels_csv["breed"].to_numpy()  # convert to Numpy array
+
+    # Find unique breeds within labels
+    unique_breeds = np.unique(labels)
+
+    # Process the uploaded image
+    process_image = image.load_img(uploaded_image, target_size=(224,224)) 
+    process_image = image.img_to_array(process_image) / 255.0
+    process_image = process_image.reshape((1, 224, 224, 3))
+    
     # Load saved model
     model = tf.keras.models.load_model('dog_breed_model_pretrained.h5', custom_objects={'KerasLayer':hub.KerasLayer})
-    
-    # Turn the uploaded image into a batch
-    # batch_image = create_data_batches(image)
-
-    resized_image = uploaded_image.resize((224, 224))
-    img_array = np.array(resized_image) / 255
-    img_array = img_array.reshape((1, 224, 224, 3))
-    
-    # Read in image file
-    # process_image = tf.io.read_file(uploaded_image)
-    # Turn the jpeg image into a numerical Tensor with 3 colours
-    # process_image = tf.image.decode_jpeg(process_image, channels=3)
-    # Convert the colour channel values from 0-225 values to 0-1 values
-    # process_image = tf.image.convert_image_dtype(process_image, tf.float32)
-    # Resize the image to our desired size (224, 224)
-    # process_image = tf.image.resize(process_image, size=[224, 224])
-        
+         
     # Make a prediction on the uploaded image
-    custom_preds = model.predict(img_array)
-    
-    
+    predictions = model.predict(process_image, batch_size=1) 
+        
     # Get image prediction labels
     labels_csv = pd.read_csv("labels.csv")
     labels = labels_csv["breed"].to_numpy()
+    unique_breeds = np.unique(labels)
+    predicted_dog = unique_breeds[np.argmax(predictions[0])]
     
-    custom_pred_labels = [get_pred_label(custom_preds[i]) for i in range(len(custom_preds))]
-  
-    
-    # Get custom images (our unbatchify() function wont work since there are not labels)
-    custom_images = []
-    # Loop through unbatched data
-    for image in custom_data.unbatch().as_numpy_iterator():
-        custom_images.append(image)
- 
-    # Check custom image predictions
-    plt.figure(figsize=(6,6))
-    for i, image in enumerate(custom_images):
-        plt.subplot()
-        plt.xticks([])
-        plt.yticks([])
-        plt.title(custom_pred_labels[i])
-        plt.imshow(image)
-
+  st.write('Predicted dog: {predicted_dog}')
   else:
     st.text('You have not uploaded an image yet.')
 
